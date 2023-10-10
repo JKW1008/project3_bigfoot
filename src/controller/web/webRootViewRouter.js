cef6e23b39b8c6b269f8446690f391f60d5adb0a
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2023-10-10 17:02:10 +0900

    유저페이지 추가

--- src/controller/web/webRootViewRouter.js
+++ src/controller/web/webRootViewRouter.js
@@ -8,7 +8,6 @@ webRootViewRouter.get("/introduce", (req, res) => res.render("introduce", {data}
 webRootViewRouter.get("/course", (req, res) => res.render("course"));
 webRootViewRouter.get("/qr", (req, res) => res.render("qr"));
 webRootViewRouter.get("/users", (req, res) => res.render("users"));
-webRootViewRouter.get("/profile", (req, res) => res.render("profile"));
 webRootViewRouter.get("/join", (req, res) => res.render("joinForm"));
 webRootViewRouter.get("/login", (req, res) => res.render("loginForm"));
 webRootViewRouter.get("/login/callback", (req, res) => res.render("loginCallback"));
@@ -19,3 +18,4 @@ webRootViewRouter.get("/tutorialStamp", (req, res) => res.render("tutorialStamp"
 webRootViewRouter.get("/tutorialRecommendation", (req, res) => res.render("tutorialRecommendation"));
 
 export default webRootViewRouter;
+
